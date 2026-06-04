51376d24904fd85701b758ec98c42cb73fa9d118
musanaldi00-design <musanaldi00@gmail.com>
2026-06-04 20:35:24 +0800

    Update create_guru.php

--- create_guru.php
+++ create_guru.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
